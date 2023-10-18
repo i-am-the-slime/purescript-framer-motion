6b0ad43d23e26da08677930aa3e8c23f0b182020
Mark Eibes <mark.eibes@gmail.com>
2023-10-18 15:29:23 +0200

    Remove accidental import

--- src/Framer/Motion/Hook.js
+++ src/Framer/Motion/Hook.js
@@ -1,9 +1,14 @@
-import { useViewportScroll, useTransform, useSpring, useAnimation } from "framer-motion"
-import {useTransformMap} from "../../../output/Framer.Motion.Hook";
+import {
+  useViewportScroll,
+  useTransform,
+  useSpring,
+  useAnimation,
+} from "framer-motion";
 
-export const useViewportScrollImpl = useViewportScroll
-export const useTransformImpl = useTransform
-export const useTransformMapImpl = (value, fn) => useTransform(value, fn)
-export const useTransformMapEImpl = (value, fnE) => useTransformMapImpl(value, fnE)
-export const useSpringImpl = useSpring
-export const useAnimationImpl = useAnimation
+export const useViewportScrollImpl = useViewportScroll;
+export const useTransformImpl = useTransform;
+export const useTransformMapImpl = (value, fn) => useTransform(value, fn);
+export const useTransformMapEImpl = (value, fnE) =>
+  useTransformMapImpl(value, fnE);
+export const useSpringImpl = useSpring;
+export const useAnimationImpl = useAnimation;
