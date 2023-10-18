13f456b567b62f1a1dca2e5465f4a8a30bb366a7
Mark Eibes <mark.eibes@gmail.com>
2023-10-18 13:43:59 +0200

    Change it again

--- src/Framer/Motion/Hook.js
+++ src/Framer/Motion/Hook.js
@@ -4,6 +4,6 @@ import {useTransformMap} from "../../../output/Framer.Motion.Hook";
 export const useViewportScrollImpl = useViewportScroll
 export const useTransformImpl = useTransform
 export const useTransformMapImpl = (value, fn) => useTransform(value, fn)
-export const useTransformMapEImpl = useTransformMapImpl
+export const useTransformMapEImpl = (value, fnE) => useTransformMapImpl(value, fnE)
 export const useSpringImpl = useSpring
 export const useAnimationImpl = useAnimation
