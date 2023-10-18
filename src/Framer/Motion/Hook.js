d81a422b428f843482a636c0979cd41268704396
Mark Eibes <mark.eibes@gmail.com>
2023-10-18 15:33:14 +0200

    Adjust function hopelessly

--- src/Framer/Motion/Hook.js
+++ src/Framer/Motion/Hook.js
@@ -8,7 +8,6 @@ import {
 export const useViewportScrollImpl = useViewportScroll;
 export const useTransformImpl = useTransform;
 export const useTransformMapImpl = (value, fn) => useTransform(value, fn);
-export const useTransformMapEImpl = (value, fnE) =>
-  useTransformMapImpl(value, fnE);
+export const useTransformMapEImpl = (value, fnE) => useTransform(value, fnE);
 export const useSpringImpl = useSpring;
 export const useAnimationImpl = useAnimation;
