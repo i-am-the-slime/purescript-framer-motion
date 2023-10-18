38021b234f93c44e1534316e6eefa8508ec4edfc
Mark Eibes <mark.eibes@gmail.com>
2023-10-18 13:41:18 +0200

    Modify useTransformMapEImpl

--- src/Framer/Motion/Hook.js
+++ src/Framer/Motion/Hook.js
@@ -1,8 +1,9 @@
 import { useViewportScroll, useTransform, useSpring, useAnimation } from "framer-motion"
+import {useTransformMap} from "../../../output/Framer.Motion.Hook";
 
 export const useViewportScrollImpl = useViewportScroll
 export const useTransformImpl = useTransform
 export const useTransformMapImpl = (value, fn) => useTransform(value, fn)
-export const useTransformMapEImpl = useTransform
+export const useTransformMapEImpl = useTransformMapImpl
 export const useSpringImpl = useSpring
 export const useAnimationImpl = useAnimation
